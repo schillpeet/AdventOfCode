c6569a0bd3355feb6ccef03dad91232e483d3bb4
schipet <schillpeet@gmail.com>
2024-12-16 08:24:16 +0100

    feat(day4): adds part 2

--- 2024/day4/day4.js
+++ 2024/day4/day4.js
@@ -1,5 +1,7 @@
 const reg = /(?=(XMAS|SAMX))/g
+
 const countOccurrencesOfXMasForwardsAndBackwards = (input) => [...input.matchAll(reg)].length
+
 function transformsColumsToRaws(input) {
     let inputArr = input.split('\n')
     // not the most elegant solution, but it allows me to continue working with a square structure
@@ -11,6 +13,7 @@ function transformsColumsToRaws(input) {
         return arr.map(elem => elem[idx]).join('')
     }).join('\n')
 }
+
 const handleDiagonal = (input, mainDiagonal) => input.split('\n').map((elem, idx, arr) => {
         const spacesEnd = '_'.repeat(arr.length * 2 - 1 - arr.length - idx)
         const spacesStart = '_'.repeat(idx)
@@ -26,8 +29,27 @@ function part1(xmasInput) {
 
     return rawsCount + columsCount + mainDiagonal + antiDiagonal
 }
-function part2() {}
+
+// Very conventional solution: illustrates the good readability and is also programmed much faster
+function part2(xmasInput) {
+    const xmasArray = xmasInput.split('\n').map(line => [...line])
+    let counter = 0
+    for (let i = 1; i < xmasArray.length - 1; i++) {
+        for (let j = 1; j < xmasArray[i].length - 1; j++) {
+            if (xmasArray[i][j] === 'A') {
+                let topLeft = xmasArray[i-1][j-1]
+                let topRight = xmasArray[i-1][j+1]
+                let bottomLeft = xmasArray[i+1][j-1]
+                let bottomRight = xmasArray[i+1][j+1]
+                let mainDiagonal = (topLeft === 'M' && bottomRight === 'S') || (topLeft === 'S' && bottomRight === 'M')
+                let antiDiagonal = (topRight === 'M' && bottomLeft === 'S') || (topRight === 'S' && bottomLeft === 'M')
+                if (mainDiagonal && antiDiagonal) counter += 1
+            }
+        }
+    }
+    return counter
+}
 
 export async function run(input, optF) {
-    return !optF ? part1(input) : part2()
+    return !optF ? part1(input) : part2(input)
 }
