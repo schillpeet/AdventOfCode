c038689b335a4c15d4a190627bd95a992900d9e0
schipet <schillpeet@gmail.com>
2024-12-17 18:20:53 +0100

    notCorrect(day5/part1): validates whether a number is in principle a predecessor or successor, but not whether it is the direct predecessor or successor

--- 2024/day5/day5.js
+++ 2024/day5/day5.js
@@ -1,53 +1,46 @@
-/**
- * The idea is as follows:
- * We have an initial list of pairs of numbers, where the first number must always 
- * come before the second number (rule). This results in the pattern that 
- * in a list of n elements, one element also has n-1 predecessors, another element 
- * has n-2 predecessors, so the pattern shows a sorted list: 0, 1, ..., n-2, n-1, n. 
- * This means that I get a key that lies at a very specific index. In the end, I only 
- * have to take my second list, map it to the result of my first list and can then say 
- * whether the second list only has descending values or not.
- * 
- * NOTES: it could probably have been solved more simply
- */
 function part1(input) {
     const [pageRulesStr, updatedPagesStr] = input.split('\n\n')
 
-    /**
-     * 1. transforms the first (Page Rule-) list to numbers
-     * 2. takes all first numbers of the pairs
-     * 3. initializes a new key if it does not yet exist or increments the value of the key
-     */
-    const counts = {}
-    pageRulesStr
-        .split(/[\|\n]/)
-        .map(Number)
-        .filter((_, idx) => idx % 2 === 0)
-        .map(num => counts[num] = counts[num] ? counts[num] + 1 : 1) // counts the occurency of this first numbers
+    const allNumbersOfRules = pageRulesStr.split(/[\|\n]/).map(Number)
+    const firstNumbersOfRules = allNumbersOfRules.filter((_, idx) => idx % 2 === 0)
+    const secNumbersOfRules = allNumbersOfRules.filter((_, idx) => idx % 2 !== 0)
+
+    const rulesSuccessor = {}
+    firstNumbersOfRules.map((key, idx) => {
+        if (!rulesSuccessor[key]) rulesSuccessor[key] = []
+        rulesSuccessor[key].push(secNumbersOfRules[idx])
+    })
     
-    /**
-     * 1. transfomrs the seconde (Update-) list to numbers
-     * 2. takes the values from the first list using the key (it returns the occurency)
-     * 3. only keeps an array if the values are sorted in descending order
-     */
-    const correctUpdates = updatedPagesStr
-        .split('\n').map(e => e.split(',').map(Number)) // array<array<number>>
-        .map(arr => arr
-            .map(value => counts[value] ? counts[value] : 0))
-            .filter(arr => arr
-                .every((num, idx) => 
-                    idx === arr.length - 1 || num > arr[idx + 1])
-            )
+    const rulesPredecessor = {}
+    secNumbersOfRules.map((key, idx) => {
+        if (!rulesPredecessor[key]) rulesPredecessor[key] = []
+        rulesPredecessor[key].push(firstNumbersOfRules[idx])
+    })
+
+    const correctUpdates = updatedPagesStr.split('\n').map(e => e.split(',').map(Number)) // array<array<Number>
+        .filter(list => {                       // array<Number>, e.g. [75,47,61,53,29]
+            const [head, ...tail] = list;       // head: [75], tail: [47,61,53,29]
+            // TODO as 5183 was too high, I will now only validate the direct successors/predecessors
+            const checkAllSuccessor = tail.every(t => rulesSuccessor[t] ? !rulesSuccessor[t].includes(head) : true);
+            
+            const [headP, ...tailP] = [...list].reverse()
+            const checkAllPredecessor = tailP.every(t => rulesPredecessor[t] ? !rulesPredecessor[t].includes(headP) : true);
+
+            console.log(checkAllSuccessor, checkAllPredecessor)
+
+            return (checkAllSuccessor && checkAllPredecessor)
+        })
     
     // returns sum of middle page numbers
     return correctUpdates.reduce((acc, listOfNumbers) => {
-        const valueToFind = listOfNumbers[(listOfNumbers.length - 1) / 2]
-        const occurencyKeyOfValue = Number(Object.keys(counts).find(key => counts[key] === valueToFind))
-        return acc += occurencyKeyOfValue
+        return acc += listOfNumbers[(listOfNumbers.length - 1) / 2]
     }, 0)
 }
 
-function part2(input) {}
+function part2(input) {
+    console.log("Not Implemented")
+    return -1
+}
 
 export async function run(input, optF) {
     return !optF ? part1(input) : part2(input)
