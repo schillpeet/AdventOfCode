5026c7fef9c525cdc581142fe75ccdaeebe53df4
schipet <schillpeet@gmail.com>
2025-01-17 19:13:54 +0100

    feat(day9): adds part2

--- 2024/day9/day9.js
+++ 2024/day9/day9.js
@@ -2,15 +2,15 @@ function part1(input) {
     const filesAndFreeSpace = input.split('').flatMap((strNum, idx) => idx % 2 === 0 
             ? [...Array(Number(strNum))].map(() => idx/2)
             : [...Array(Number(strNum))].map(() => '.')
-    );
+    )
 
     const filesAndFreeSpaceReverse = [...filesAndFreeSpace].reverse().flatMap(elem => typeof elem === 'number' ? elem: [])
 
     const numOfDots = filesAndFreeSpace.reduce((acc, val) => val === '.' ? acc + 1 : acc, 0)
 
     const defragmentation = filesAndFreeSpace.flatMap(num => typeof num === 'number' ? num
-        : (filesAndFreeSpaceReverse[0] && filesAndFreeSpaceReverse.splice(0,1))
-    )
+        : (filesAndFreeSpaceReverse[0] && filesAndFreeSpaceReverse.shift()))
+
     defragmentation.splice( (-1) * numOfDots)
 
     const checksum = defragmentation.reduce((acc, value, idx) => acc += value * idx, 0)
@@ -19,9 +19,59 @@ function part1(input) {
 }
 
 function part2(input) {
-    console.error("Not implemented yet!")    
-    return -1
+    let entries = input.split('').map((l,i) => {
+        const isFile = i % 2 === 0
+        const hasValue = Number(l)
+        const idx = !isFile ? NaN : i/2
+        return { file: isFile, value: hasValue, origIdx: idx }
+    })
+
+    let ls = []
+
+    // adds element to ls list and removes from origin entries list
+    function addsFilesOrFreeMem() {
+        ls.unshift(entries.at(-1))
+        entries.pop()
+    }
+    
+    while(entries.length > 0) {
+        // adds free space to the list or takes the last file element
+        let lastFile = entries.at(-1)
+        if (!lastFile.file || lastFile.isMoved || lastFile.value === 0 && !lastFile.file ) { addsFilesOrFreeMem(); continue }
+        
+        // validates if free mem exists for this file or adds this file
+        let freeMemIdx = entries.findIndex(fm => !fm.file && fm.value >= lastFile.value)
+        if (freeMemIdx === -1) { addsFilesOrFreeMem(); continue }
+        
+        // transforms lastFile space to free space and adds it to the list
+        entries.slice(-1)[0].file = false
+        addsFilesOrFreeMem()
+        
+        // transform freeMem to file completely
+        if (entries[freeMemIdx].value === lastFile.value) {
+            entries[freeMemIdx].file = true
+            entries[freeMemIdx].isMoved = true
+            entries[freeMemIdx].origIdx = lastFile.origIdx
+            continue
+        }
+        
+        // split free mem in file and free mem
+        const copyFile = { file: true, value: lastFile.value, isMoved: true, origIdx: lastFile.origIdx }
+        entries[freeMemIdx].value -= copyFile.value
+        entries.splice(freeMemIdx, 0, copyFile)
+    }
+
+    // transforms the list of entities to the list like in the example: [1234] => [0..222....] 
+    // (or: [0002220000], or zeros, as these are not counted)
+    const visualList = ls.map(e => e.file ? Array(e.value).fill(e.origIdx) : Array(e.value).fill(0)).flat()
+
+    let checksum = visualList
+        .map((elem, i) =>  elem * i)
+        .reduce((acc,value) => acc += value ,0)
+    
+    return checksum
 }
+
 
 /**
  * @param {String} input - including line breaks
