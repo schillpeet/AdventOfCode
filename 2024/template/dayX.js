e16fcc171abba3f01f08288c3adc736363db0661
schipet <schillpeet@gmail.com>
2025-01-18 16:01:45 +0100

    chore: adds jsconfig and allows IntelliSense to make auto-complete suggestions

--- 2024/template/dayX.js
+++ 2024/template/dayX.js
@@ -1,14 +1,25 @@
+/**
+ * JSDoc comments help IntelliSense to recognize the right types and make suggestions
+ * @param {String} input 
+ * @returns 
+ */
 function part1(input) { 
     console.error("Not implemented yet!")
     return -1
 }
 
+/**
+ * JSDoc comments help IntelliSense to recognize the right types and make suggestions
+ * @param {String} input 
+ * @returns 
+ */
 function part2(input) {
     console.error("Not implemented yet!")    
     return -1
 }
 
 /**
+ * JSDoc comments help IntelliSense to recognize the right types and make suggestions
  * @param {String} input - including line breaks
  * @param {Boolean} optF - option final = true, means execute part 2
  * @returns 
