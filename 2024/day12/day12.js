14b1d8a9671b595121a5d99fbbefe3bdfccaf663
schipet <schillpeet@gmail.com>
2025-02-16 11:40:32 +0100

    feat(day12): adds part2

--- 2024/day12/day12.js
+++ 2024/day12/day12.js
@@ -6,7 +6,7 @@ const hasNeighbour = (x1,y1,x2,y2) => Math.abs(x1 - x2) === 1 && y1 === y2 || Ma
  * @param {[[string, [number, number], number]]} rest 
  * @returns 
  */
-function niceFunctionName(todos, rest) {
+function calcEachRegion(todos, rest) {
     let todoList = [...todos]
     let finList = []
 
@@ -14,14 +14,12 @@ function niceFunctionName(todos, rest) {
         let head = todoList.shift()
         finList.push(head)
 
-        /**
-         * all neighbours that are not in the finish list and not in the todo list as well
-         */
+        // all neighbours that are not in the finish list and not in the todo list as well
         const allNeighbours = rest.filter(e => hasNeighbour(head[1].at(0), head[1].at(1), e[1].at(0), e[1].at(1)))
             .filter(e => !finList.some(s => s[1].at(0) === e[1].at(0) && s[1].at(1) === e[1].at(1)))
             .filter(e => !todoList.some(s => s[1].at(0) === e[1].at(0) && s[1].at(1) === e[1].at(1)))
+        
         allNeighbours.forEach(n => todoList.push(n))
-
     }
     return finList
 }
@@ -31,12 +29,40 @@ function calculateRegions(list, result) {
 
     const myHead = [head]
     // @ts-ignore
-    const oneRegion = niceFunctionName(myHead, tail)
+    const oneRegion = calcEachRegion(myHead, tail)
 
     const restPlants = list.filter(e => !oneRegion.includes(e))
     result.push(oneRegion)
     if (restPlants.length === 0) return result
     return calculateRegions(restPlants, result)
+}
+
+const getGardenMapPart1 = input => input
+    .split('\n').map(line => line.split(''))
+    .flatMap((arr, ai) => arr.map((elem, ei) => [elem, [ai,ei], 4]))
+
+const getGardenMapPart2 = input => input
+    .split('\n').map(line => line.split(''))
+    .flatMap((arr, ai) => arr.map((elem, ei) => [elem, [ai,ei], [], []]))
+
+const getMapToGetAllPlants = input => input.split('\n').flatMap(line => line.split(''))
+
+const getAllKindsOfPlants = mapToGetAllPlants => mapToGetAllPlants.filter((e,i) => mapToGetAllPlants.indexOf(e) === i)
+
+function calcRegionsForAllKindsOfPlants(allKindsOfPlants, gardenMap) {
+    let result = []
+    allKindsOfPlants.forEach(plant => {
+        const specificPlant = gardenMap.filter(e => e[0][0] === plant)
+        calculateRegions(specificPlant, result)
+    })
+    return result
+}
+
+function getAllRegions(input, part) {
+    const gardenMap = part === 1 ? getGardenMapPart1(input) : getGardenMapPart2(input)   
+    const mapToGetAllPlants = getMapToGetAllPlants(input)
+    const allKindsOfPlants = getAllKindsOfPlants(mapToGetAllPlants)
+    return calcRegionsForAllKindsOfPlants(allKindsOfPlants, gardenMap)
 }
 
 /**
@@ -45,23 +71,10 @@ function calculateRegions(list, result) {
  * @returns 
  */
 function part1(input) { 
-    const gardenMap = input
-        .split('\n').map(line => line.split(''))
-        .flatMap((arr, ai) => arr.map((elem, ei) => [elem, [ai,ei], 4]))       
-    
-    const theTruth = input
-        .split('\n').flatMap(line => line.split(''))
-    
-    const allKindsOfPlants = theTruth.filter((e,i) => theTruth.indexOf(e) === i)
-    
-    let result = []
-    allKindsOfPlants.forEach(plant => {
-        const specificPlant = gardenMap.filter(e => e[0][0] === plant)
-        calculateRegions(specificPlant, result)
-    })
+    const allRegions = getAllRegions(input, 1)
     
     // calculate fences per entry
-    result.forEach(plantArea => {
+    allRegions.forEach(plantArea => {
         plantArea.map(p => {
             const numOfNeighbours = plantArea.filter(plant => hasNeighbour(p[1][0],p[1][1],plant[1][0], plant[1][1])).length
             p[2] -= numOfNeighbours
@@ -70,7 +83,7 @@ function part1(input) {
     })
     
     let costs = 0
-    result.forEach(plantArea => {
+    allRegions.forEach(plantArea => {
         const numOfPlantsInArea = plantArea.length
         const costsPerArea = plantArea.reduce((acc,value) => {
             const costsPerPlant = value[2] * numOfPlantsInArea
@@ -82,14 +95,107 @@ function part1(input) {
     return costs
 }
 
+function getAllRegionsPart2(allRegions) {
+    allRegions.forEach(plantArea => {
+        plantArea.map(p => {
+            // add coordinates of each neighbour
+            const neighbours = plantArea.filter(plant => hasNeighbour(p[1][0],p[1][1],plant[1][0], plant[1][1]))
+            neighbours.forEach(n => p[2].push(n.at(1)))
+            // add N,E,S,W = border: N is North and means that there is a free area for a fence above the current element
+            if (!p[2].some(n => n.at(0) === p[1].at(0) - 1)) p[3].push('N')
+            if (!p[2].some(n => n.at(1) === p[1].at(1) + 1)) p[3].push('E')
+            if (!p[2].some(n => n.at(0) === p[1].at(0) + 1)) p[3].push('S')
+            if (!p[2].some(n => n.at(1) === p[1].at(1) - 1)) p[3].push('W')
+
+            return p
+        })
+    })
+    return allRegions
+}
+
+// north south
+function groupByLevelVertical(entries) {
+    return Object.values(entries.reduce((acc, [plantName, [x,y], neighbours, plantBorder]) => {
+        if (!acc[x]) acc[x] = []
+        acc[x].push([plantName, [x,y], neighbours, plantBorder])
+        return acc
+    }, {}))
+}
+// east west
+function groupByLevelHorizontal(entries) {
+    return Object.values(entries.reduce((acc, [plantName, [x,y], neighbours, plantBorder]) => {
+        if (!acc[y]) acc[y] = []
+        acc[y].push([plantName, [x,y], neighbours, plantBorder])
+        return acc
+    }, {}))
+}
+function groupByLinesVertical(entries) {
+    const sortLines = entries.map(groups => groups.sort((a,b) => a[1][1] - b[1][1]))
+    let reduceSortedList = []
+    sortLines.forEach(groups => {
+        let tmpRed = groups.reduce((acc, value) => {
+            if (acc.length === 0) acc.push([value])
+            else {
+                const lastEntryValue = acc.at(-1).at(-1)[1][1]
+                const curEntryValue = value[1][1]
+                if (lastEntryValue === curEntryValue - 1) acc.at(-1).push(value)
+                else acc.push([value])
+            }
+            return acc
+        }, [])
+        reduceSortedList.push(tmpRed)
+    })
+    return reduceSortedList
+}
+
+// east west
+function groupByLinesHorizontal(entries) {
+    const sortLines = entries.map(groups => groups.sort((a,b) => a[0][0] - b[0][0])) // sort by y
+    let reduceSortedList = []
+    sortLines.forEach(groups => {
+        let tmpRed = groups.reduce((acc, value) => {
+            if (acc.length === 0) acc.push([value])
+            else {
+                const findLineNeighbourIdx = acc.findIndex(lineGroup => lineGroup.some(lineEntry => 
+                    lineEntry[1][1] === value[1][1] &&
+                    lineEntry[1][0] === value[1][0] - 1 ||
+                    lineEntry[1][0] === value[1][0] + 1
+                ))
+                if (findLineNeighbourIdx !== -1) acc[findLineNeighbourIdx].push(value)
+                else acc.push([value])
+            }
+            return acc
+        }, [])
+        reduceSortedList.push(tmpRed)
+    })
+    return reduceSortedList
+}
+function countLines(entries) {
+    const result = entries.reduce((total, group) => 
+        total + group.reduce((sum, ) => sum += 1, 0)
+    , 0)
+    return result
+}
+
+function calcBorders(regionsWithBorderAnnotation) {
+    const northCount = countLines(groupByLinesVertical(groupByLevelVertical(regionsWithBorderAnnotation.filter(elem => elem.at(3).includes('N')))))
+    const southCount = countLines(groupByLinesVertical(groupByLevelVertical(regionsWithBorderAnnotation.filter(elem => elem.at(3).includes('S')))))
+
+    const eastCount = countLines(groupByLinesHorizontal(groupByLevelHorizontal(regionsWithBorderAnnotation.filter(elem => elem.at(3).includes('E')))))
+    const westCount = countLines(groupByLinesHorizontal(groupByLevelHorizontal(regionsWithBorderAnnotation.filter(elem => elem.at(3).includes('W')))))
+
+    return northCount + eastCount + southCount + westCount
+}
+
 /**
  * JSDoc comments help IntelliSense to recognize the right types and make suggestions
  * @param {string} input 
  * @returns 
  */
 function part2(input) {
-    console.error("Not implemented yet!", input)    
-    return -1
+    const allRegions = getAllRegions(input, 2)
+    const regionsWithBorderAnnotation = getAllRegionsPart2([...allRegions])
+    return regionsWithBorderAnnotation.reduce((acc, region) => acc += calcBorders(region) * region.length, 0);
 }
 
 /**
@@ -102,4 +208,4 @@ export async function run(input, optF) {
     return !optF ? part1(input) : part2(input)
 }
 
-export { niceFunctionName }
+export { calcEachRegion, part2, getAllRegions, getAllRegionsPart2 }
--- 2024/day12/day12.test.js
+++ 2024/day12/day12.test.js
@@ -1,18 +1,19 @@
 import { describe, expect, it } from "vitest";
-import { niceFunctionName } from "./day12.js";
+import { calcEachRegion, getAllRegions, getAllRegionsPart2, part2 } from "./day12.js";
 
-/* const EXAMPLE_1 = 'AAAA\nBBCD\nBBCC\nEEEC'
+const EXAMPLE_1 = 'AAAA\nBBCD\nBBCC\nEEEC'
 const EXAMPLE_2 = 'OOOOO\nOXOXO\nOOOOO\nOXOXO\nOOOOO'
 const EXAMPLE_3 = 'RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE'
- */
-describe('Advent of Code Tests: day 11, part', () => {
+const EXAMPLE_4 = 'AAAAAA\nAAABBA\nAAABBA\nABBAAA\nABBAAA\nAAAAAA'
+
+describe('Advent of Code Tests: day 11, part 1', () => {
     
     it('is a simple test that should return a group of plants', () => {
         const todos = [['A', [0,0], 4]] 
         const rest = [['A', [0,1], 4], ['A', [1,0], 4]]
         const exp = [['A', [0,0], 4], ['A', [0,1], 4], ['A', [1,0], 4]]
         // @ts-ignore 
-        const output = niceFunctionName(todos, rest)
+        const output = calcEachRegion(todos, rest)
         
         expect(output).toStrictEqual(exp)
     })
@@ -23,7 +24,7 @@ describe('Advent of Code Tests: day 11, part', () => {
         const exp = [['A', [0,0], 4], ['A', [0,1], 4], ['A', [1,0], 4]]
 
         // @ts-ignore
-        const output = niceFunctionName(todos, rest)
+        const output = calcEachRegion(todos, rest)
 
         expect(output).toStrictEqual(exp)
     })
@@ -39,7 +40,7 @@ describe('Advent of Code Tests: day 11, part', () => {
         const exp = [['A', [0,0], 4], ['A', [0,1], 4], ['A', [1,0], 4], ['A', [2,0], 4], ['A', [2,1], 4], ['A', [2,2], 4], ['A', [1,2], 4]]
 
         // @ts-ignore
-        const output = niceFunctionName(todos, rest)
+        const output = calcEachRegion(todos, rest)
 
         expect(output).toStrictEqual(exp)
     })
@@ -49,15 +50,142 @@ describe('Advent of Code Tests: day 11, part', () => {
      *      XAA
      *      XAA
      */
-        it('should calculate the right area on the right side', () => {
-            const todos = [['A', [0,2], 4]]
-            const rest = [['A', [1,1], 4], ['A', [1,2], 4], ['A', [2,1], 4], ['A', [2,2], 4]]
-            const exp = [['A', [0,2], 4], ['A', [1,2], 4], ['A', [1,1], 4], ['A', [2,2], 4], ['A', [2,1], 4]]
+    it('should calculate the right area on the right side', () => {
+        const todos = [['A', [0,2], 4]]
+        const rest = [['A', [1,1], 4], ['A', [1,2], 4], ['A', [2,1], 4], ['A', [2,2], 4]]
+        const exp = [['A', [0,2], 4], ['A', [1,2], 4], ['A', [1,1], 4], ['A', [2,2], 4], ['A', [2,1], 4]]
+
+        // @ts-ignore
+        const output = calcEachRegion(todos, rest)
+
+        expect(output).toStrictEqual(exp)
+    })
+})
+
+describe('Advent of Code Tests: day 11, part 2', () => {
+
+    it('should return all Areas of plants', () => {
+        const input = "AA\nAA"
+        const allRegions = getAllRegions(input, 2)
+        const exp = [[
+            ['A', [0,0], [], []], 
+            ['A', [0,1], [], []], 
+            ['A', [1,0], [], []], 
+            ['A', [1,1], [], []]
+        ]]
+        expect(allRegions).toStrictEqual(exp)
+    })
     
-            // @ts-ignore
-            const output = niceFunctionName(todos, rest)
+    it('should return a list of plants with the information of the neighbours coordinates and the border in form of N,E,S,W', () => {
+        const input = "AA\nAA"
+        const allRegions = getAllRegions(input, 2)
+        const regionsWithBorderAnnotation = getAllRegionsPart2(allRegions)
+        const exp = [[
+                        ['A', [0,0], [[0,1],[1,0]], ['N','W']], 
+                        ['A', [0,1], [[0,0],[1,1]], ['N', 'E']], 
+                        ['A', [1,0], [[0,0],[1,1]], ['S', 'W']], 
+                        ['A', [1,1], [[0,1],[1,0]], ['E', 'S']]
+                    ]]
+
+        expect(regionsWithBorderAnnotation).toStrictEqual(exp)
+    })
+
+    it.skip('should return another list of plants with the information of the neighbours coordinates and the border in form of N,E,S,W', () => {
+        const input = "AA\nBB\nAA"
+        const allRegions = getAllRegions(input, 2)
+        const regionsWithBorderAnnotation = getAllRegionsPart2(allRegions)
+        const exp = [[
+                        ['A', [0,0], [[0,1],[1,0]], ['N','W']],     ['A', [0,1], [[0,0],[1,1]], ['N', 'E']]
+                    ],[
+                        ['B', [1,0], [[0,0],[0,1],[1,1]], ['E']],   ['B', [1,1], [[0,1],[1,0],[2,1]], ['W']]
+                    ],[
+                        ['A', [2,0], [[1,0],[2,1]], ['S','W']],     ['A', [2,1], [[2,0],[1,1]], ['E', 'S']]
+                    ]]
+
+        expect(regionsWithBorderAnnotation).toStrictEqual(exp)
+    })
+
+    it('should validate the example input 1', () => {
+        const exampleResult = part2(EXAMPLE_1)
+        expect(exampleResult).toBe(80)
+    })
+
+    it('should validate the example input 2', () => {
+        const exampleResult = part2(EXAMPLE_2)
+        expect(exampleResult).toBe(436)
+    })
+
+    it('should validate the example input 3', () => {
+        const exampleResult = part2(EXAMPLE_3)
+        expect(exampleResult).toBe(1206)
+    })
+
+    it('should validate the example input 4', () => {
+        const exampleResult = part2(EXAMPLE_4)
+        expect(exampleResult).toBe(368)
+    })
     
-            expect(output).toStrictEqual(exp)
-        })
+
+    it('should validate number of regions with borders', () => {
+        const allRegions = getAllRegions(EXAMPLE_4, 2)
+        const regionsWithBorderAnnotation = getAllRegionsPart2([...allRegions])
+        const lengthResult = regionsWithBorderAnnotation.length
+        expect(lengthResult).toBe(3)
+    })
+
+    // further tests
+    it('simple test', () => {
+        const input = "AA\nBB\nAA"
+        const output = part2(input)
+        expect(output).toBe(24)
+    })
+
+    it('simple test 2', () => {
+        const input = "ABA\nBBB\nBAB\nBBB"
+        const output = part2(input)
+        expect(output).toBe(120)
+    })
+
+    it('test a snake', () => {
+        const input = "AAAAAB\nABBBAA\nABAABA\nABABAA\nABAAAB\nAABBBB"
+        const output = part2(input)
+        expect(output).toBe(606)
+    })
+
+    it('tests another snake', () => {
+        const input = "AAAAAB\nABBBAA\nABBABA\nABABAA\nABAAAB\nAABBBB"
+        const output = part2(input)
+        expect(output).toBe(542)
+    })
+
+    it('tests another ', () => {
+        const input = "AAAAA\nABABA\nAAAAA"
+        const output = part2(input)
+        expect(output).toBe(164) 
+    })
+    it('tests another 2', () => {
+        const input = "ABA\nBAB\nABA"
+        const output = part2(input)
+        expect(output).toBe(36) 
+    })
+    it('tests a special W in input file', () => {
+        const input = `JJJJJWJEE
+JJJJWWJEE
+JJWJWWWWE
+JJWWWWWEE
+JWWWWWEEE
+JJJWWWEEE
+JJJJWWWJE
+JJJJWJWJE
+JJJJJJJJE`
+        const output = part2(input)
+        expect(output).toBe(1816)
+    })
+
+    it('should test the fences in all directions', () => {
+        const input = 'AAxAAA\nAAxAAA\nxAAAxx\nAAxAAA\nAAxAAA\nAAxAAA'
+        const output = part2(input)
+        expect(output).toBe(592)
+    })
 
 })
