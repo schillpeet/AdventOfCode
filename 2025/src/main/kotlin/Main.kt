cbc7f03930836bfae9908c63ed8b55df32e878fa
schipet <schillpeet@gmail.com>
2025-12-02 14:02:38 +0100

    aoc 2025 basic initialization

---
+++ 2025/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package org.example
+
+import org.example.days.day1.day1
+
+fun main() {
+    println(day1().part1())
+}
---
+++ 2025/src/main/kotlin/days/day1/day1.kt
@@ -0,0 +1,7 @@
+package org.example.days.day1
+
+class day1() {
+    fun part1() {}
+    fun part2() {}
+
+}
