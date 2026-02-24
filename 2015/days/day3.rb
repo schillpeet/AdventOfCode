8b6f1359fa91cdb9a074e52642b7289220f70601
schipet <schillpeet@gmail.com>
2026-02-24 23:54:57 +0100

    aoc(2015/day3): adds part2

--- 2015/days/day3.rb
+++ 2015/days/day3.rb
@@ -1,18 +1,18 @@
 input = File.read("puzzles/day3.txt")
 
-def part1(input)
-  x, y = 0, 0
-  visited = [[0,0]]
-
-  moves = {
+MOVES = {
     "^" => [1, 0],
     "v" => [-1, 0],
     ">" => [0, 1],
     "<" => [0, -1]
   }
+
+def part1(input)
+  x, y = 0, 0
+  visited = [[0,0]]
   
   input.each_char do |c|
-    dx, dy = moves[c]
+    dx, dy = MOVES[c]
     x += dx
     y += dy
     visited << [x,y]
@@ -22,3 +22,24 @@ def part1(input)
 end
 
 puts "day3, part1: #{part1(input)}"
+
+def part2(input)
+  santa = [0,0]
+  robot = [0,0]
+  visited = [[0,0]]
+  
+  input.each_char.with_index do |c,i|
+    dx, dy = MOVES[c]
+
+    active_mover = i.even? ? santa : robot
+    
+    active_mover[0] += dx
+    active_mover[1] += dy
+
+    visited << active_mover.dup
+  end
+
+  visited.uniq.size
+end
+
+puts "day3, part2: #{part2(input)}"
