e4d3fc72d40b39dacc4f94f68cf34bd949c207b9
schipet <schillpeet@gmail.com>
2026-02-24 21:59:02 +0100

    aoc(2015/day1): part2

--- 2015/day1.rb
+++ 2015/day1.rb
@@ -1,5 +1,19 @@
 input = File.read("day1") # read opens and closes file
-#input = "(())"
 
-result = input.each_char.sum { |c| c == '(' ? 1 : -1}
-print result
+# @param input [String]
+def part1(input)
+  input.each_char.sum { |c| c == '(' ? 1 : -1}
+end
+
+puts "day1, part1: #{part1(input)}"
+
+# @param input [String]
+def part2(input)
+  sum = 0
+  input.each_char.with_index(1) do  |c,i| 
+    sum += c == '(' ? 1 : -1
+    return i if sum == -1
+  end
+end
+
+puts "day1, part2: #{part2(input)}"
