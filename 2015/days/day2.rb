b450ee02b73a99911dd98d58e642a8af7ab901e6
schipet <schillpeet@gmail.com>
2026-02-24 22:27:59 +0100

    aoc(2015/day2): adds part1

--- 2015/days/day2.rb
+++ 2015/days/day2.rb
@@ -1,7 +1,12 @@
-input = File("days/day2.txt")
+input = File.read("puzzles/day2.txt")
 
 def part1(input)
-  
+  input.each_line.sum do |line|
+    a, b, c = line.split("x").map(&:to_i) # destructuring + map to int
+    area = 2*a*b + 2*b*c + 2*c*a
+    sides = [a*b, b*c, c*a].min
+    area + sides
+  end
 end
 
 puts "day2, part1: #{part1(input)}"
