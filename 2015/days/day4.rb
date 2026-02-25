947f57937efc5ed43d33264e776c87c3130d9ef8
schipet <schillpeet@gmail.com>
2026-02-25 13:09:27 +0100

    aoc(2015/day4): adds day4 part2

--- 2015/days/day4.rb
+++ 2015/days/day4.rb
@@ -1,14 +1,19 @@
 require 'digest'
 
-input = "bgvyzdsv"
 input_test_1 = "abcdef"
 
-
-count = 0
-loop do
-  result = Digest::MD5.hexdigest("#{input}#{count}")
-  break if result.start_with?("00000")
-  count += 1
+def part1and2(input, difficulty)
+  count = 0
+  prefix = difficulty == 1 ? "00000" : "000000"
+  loop do
+    result = Digest::MD5.hexdigest("#{input}#{count}")
+    return count if result.start_with?(prefix)
+    count += 1
+  end
+  count
 end
 
-puts "day1, part1: #{count}" #     #{part1(input)}"
+input = "bgvyzdsv"
+
+puts "day1, part1: #{part1and2(input, 1)}"
+puts "day1, part2: #{part1and2(input, 2)}"
